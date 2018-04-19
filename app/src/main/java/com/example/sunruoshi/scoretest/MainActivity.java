b1c8e2de508721c02c850835bc0099a2f4655636
Bomanx <boman@bu.edu>
2018-04-18 22:06:19 -0400

    Minor changes

--- app/src/main/java/com/example/sunruoshi/scoretest/MainActivity.java
+++ app/src/main/java/com/example/sunruoshi/scoretest/MainActivity.java
@@ -221,20 +221,21 @@ public class MainActivity extends AppCompatActivity {
                 //Toast.makeText(getApplicationContext(), "Your final score is: " + compute.getScore(), Toast.LENGTH_LONG).show();
                 Toast.makeText(getApplicationContext(), "Your final score is: " + total_score, Toast.LENGTH_LONG).show();
                 currentUser = FirebaseAuth.getInstance().getCurrentUser();
-                myRef.child("Users").child(currentUser.getUid()).child("score").addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        int curScore = dataSnapshot.getValue(Integer.class);
-                        if(total_score>curScore){
-                            myRef.child("Users").child(currentUser.getUid()).child("score").setValue(total_score);
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
+                myRef.child("Users").child(currentUser.getUid()).child("score").setValue(total_score);
+//                myRef.child("Users").child(currentUser.getUid()).child("score").addValueEventListener(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(DataSnapshot dataSnapshot) {
+//                        int curScore = dataSnapshot.getValue(Integer.class);
+//                        if(total_score>curScore){
+//                            myRef.child("Users").child(currentUser.getUid()).child("score").setValue(total_score);
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(DatabaseError databaseError) {
+//
+//                    }
+//                });
                 //compute.setCount(0);
                 count = 0;
                 //compute.setScore(0);
